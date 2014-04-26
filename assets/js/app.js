e72746b8149f84688532da6ba3d5edc3b588d051
Kolja <kolja@apfel.de>
2014-04-26 13:07:01 +0200

    controller now being called

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,17 +3,14 @@ var timepuncher = angular.module('timepuncher', ['ngRoute'])
 
     .controller('TimepuncherController', function($scope,$routeParams) {
         $scope.params = $routeParams;
-        $scope.data = {message: "Hello"};
-        console.log($scope);
-        console.log($routeParams);
+        $scope.data = {message: "blork"};
     })
 
     .config( function ($routeProvider) {
-        console.log("config");
         $routeProvider
             .when( '/', {
                 controller: 'TimepuncherController',
-                templateUrl: 'partials/card.html'
+                templateUrl: 'views/main.html'
             })
             .otherwise( function ($scope) {
                 console.log("blork");

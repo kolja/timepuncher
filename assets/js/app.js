f61e935cae170a1e5f98adf281ed031d8f4467a5
Kolja <kolja@apfel.de>
2014-05-01 11:50:47 +0200

    adding a factory; wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,29 @@
-var timepuncherAPI = angular.module("timepuncherAPI", ["ngResource"]);
 
-service.factory( "card", function ($resource) {
+var tp = angular.module('timepuncher', ['ngRoute','ngResource'])
+
+tp.factory('cardLocal', function () {
+    var STORAGE_ID = 'timepuncher';
+
+    return {
+        get: function () {
+            return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+        },
+        put: function (cards) {
+            localStorage.setItem(STORAGE_ID, JSON.stringify(cards));
+        }
+    };
+});
+
+tp.factory( "cardCouch", function ($resource) {
     return $resource(
         'http://127.0.0.1:5984/timepuncher/a444c37b2521fc0f59fdf65b4b000e52', {}, {
             update: {
                 method: 'PUT'
             },
-            reviews: {
+            cards: {
                 method: 'GET',
                 params: {
-                    reviews_only: true
+                    active_only: true
                 },
                 isArray: true
             }
@@ -17,24 +31,22 @@ service.factory( "card", function ($resource) {
     );
 });
 
-
-var timepuncher = angular.module('timepuncher', ['ngRoute'])
-    .controller('TimepuncherController', function($scope,$routeParams,$http) {
-        $scope.params = $routeParams;
-        $http.get('http://127.0.0.1:5984/timepuncher/a444c37b2521fc0f59fdf65b4b000e52').success(function(data) {
-            $scope.data = data;
-            console.log(data);
-        });
-    })
-
-    .config( function ($routeProvider) {
-        $routeProvider
-            .when( '/', {
-                controller: 'TimepuncherController',
-                templateUrl: 'views/main.html'
-            })
-            .otherwise( function ($scope) {
-                console.log("blork");
-            });
+tp.controller('TimepuncherController', function($scope,$routeParams,$http) {
+    $scope.params = $routeParams;
+    $http.get('http://127.0.0.1:5984/timepuncher/a444c37b2521fc0f59fdf65b4b000e52').success(function(data) {
+        $scope.data = data;
+        console.log(data);
     });
+});
+
+tp.config( function ($routeProvider) {
+    $routeProvider
+        .when( '/', {
+            controller: 'TimepuncherController',
+            templateUrl: 'views/main.html'
+        })
+        .otherwise( function ($scope) {
+            console.log("blork");
+        });
+});
 

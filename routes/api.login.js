79177e8dbb724d532df46f63ce5841c3c855371c
Kolja <kolja@apfel.de>
2014-05-24 14:01:12 +0200

    fix login route, wip

--- routes/api.login.js
+++ routes/api.login.js
@@ -7,20 +7,24 @@ nano = require('nano')({url: "http://localhost:5984"});
 
 module.exports = function(req, res){
 
+    var cookies = {};
     var login = req.body.name;
     var password = req.body.password;
+    var callback = function ( err, message ) {
+        if (err) { res.send(message + ": " + err.reason); return; };
+        res.send(message);
+    };
 
-    nano.request({
-        method: "post",
-        path: "_session",
-        content_type: "application/json",
-        headers: {
-            name: login,
-            password: password
+    nano.auth(login, password, function (err, body, headers) {
+        if (err) { return callback(err, "login failed"); }
+
+        if (headers && headers['set-cookie']) {
+            cookies[login] = headers['set-cookie'];
         }
-    }, function (err, body, headers) {
-        if (err) { res.send(err.reason); return; }
+        console.log(body);
+        console.log(headers);
 
-        res.send("You are logged in! ");
+        callback(null, "it worked");
     });
+
 };
